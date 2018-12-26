5ac9453344be0de92c12b2b866b927519ece448f
dashohoxha <dashohoxha@gmail.com>
2018-12-26 03:12:57 +0100

    Mark package mint-meta-mate as manually installed

--- tessa/script/install-desktops.sh
+++ tessa/script/install-desktops.sh
@@ -4,4 +4,11 @@ echo '### Installing desktops ###'
 apt install --yes --install-recommends \
     mint-meta-xfce \
     mint-meta-mate \
-    mint-meta-cinnamon
+    mint-meta-cinnamon \
+    mint-meta-codecs
+
+apt-mark manual \
+    mint-meta-xfce \
+    mint-meta-mate \
+    mint-meta-cinnamon \
+    mint-meta-codecs
